72f8c04a881082eb989d00c601c01ddcce70d188
Sin <mrsin0916@gmail.com>
2019-12-24 16:43:00 +0700

    Improve Header for Blog
    Add html, css, js, responsive

--- resources/js/responsive.js
+++ resources/js/responsive.js
@@ -2,6 +2,19 @@
 var headerContainer = document.getElementById('header-container');
 
 headerTogger = () => {
+  headerContainer.classList.toggle('responsive');
+};
+
+window.onresize = () => {
+  if (window.innerWidth >= 720) {
+    headerContainer.classList.remove('responsive');
+  }
+};
+
+/* Header */
+var headerContainer = document.getElementById('header-container');
+
+headerTogger = () => {
   let value = headerContainer.classList.value;
   if (value == '' || value == 'slide-up') {
     headerContainer.classList.remove('slide-up');
