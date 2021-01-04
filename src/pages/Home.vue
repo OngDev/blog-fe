71f696851a52fa6b7ae2ac5b4551ebee380cb514
ngvuthanhnhan <ngvuthanhnhan@gmail.com>
2021-01-04 23:32:37 +0700

    init Vue project

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <MyLayout />
+  </div>
+</template>
+
+<script>
+import MyLayout from "../components/MyLayout";
+export default {
+  name: "Home",
+  components: {
+    MyLayout,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
