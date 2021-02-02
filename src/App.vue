589e83587ca7cacfbdf14dee70c4aa88ab2c627a
Khanh Qui <vkhanhqui@gmail.com>
2021-02-02 22:13:07 +0700

    add footer, fonts folder

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <router-view />
+  <Footer />
 </template>
 
 <script>
+import Footer from "./pages/Footer.vue";
 import store from "./store/index";
+
 export default {
+  components: { Footer },
   data: () => ({
     shareState: store.state,
   }),
---
+++ src/pages/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <div class="copyright">
+      2020 © Ong Dev Project.<br />All rights reserved.
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@font-face {
+  font-family: "Avenir Next";
+  src: url("../fonts/AvenirNextLTPro-Regular.otf");
+}
+footer {
+  background-color: #312c2e;
+  width: 100%;
+  height: 80px;
+}
+.copyright {
+  height: 16px;
+  color: #fafafa;
+  font-family: "Avenir Next";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1em;
+  line-height: 16px;
+  padding: 16px 16px;
+  display: inline-block;
+  margin: 6px 50px 26px 50px;
+}
+</style>
